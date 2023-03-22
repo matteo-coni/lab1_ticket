541e57dc5cee6c37b7c036b7a222a30572deb648
Brauzi35 <57174753+Brauzi35@users.noreply.github.com>
2023-03-22 16:49:47 +0100

    daje roma daje

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("Daje Roma Daje pd");
+        System.out.println("Daje Roma Daje pd ladro cane");
     }
 }
